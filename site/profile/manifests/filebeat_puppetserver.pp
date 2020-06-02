073157f8ce4d0d0b6b19d151ea84ea8f9c2d23bd
root <root@puppet.local>
2020-06-02 04:21:31 +0000

    Add filebeat_puppetserver.pp and update master.pp

---
+++ site/profile/manifests/filebeat_puppetserver.pp
@@ -0,0 +1,13 @@
+class profile::filebeat_puppetserver {
+  class { 'elk::filebeat':
+    prospector => [ {
+     "type" => "log",
+     "paths" => [
+        "/var/log/puppetlabs/puppetserver/puppetserver.log.json",
+        "/var/log/puppetlabs/puppetserver/puppetserver-access.log.json",
+      ],
+     "json_keys_under_root" => true
+    } ]
+  logstash_server => 'elk.local', 
+  }
+}
